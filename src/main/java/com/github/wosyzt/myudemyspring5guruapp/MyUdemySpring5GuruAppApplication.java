bfc2a12cf52c1673b276d78770332442c462df6b
wosyzt <wosyzt@outlook.com>
2021-12-09 11:27:34 +0800

    Initial commit

---
+++ src/main/java/com/github/wosyzt/myudemyspring5guruapp/MyUdemySpring5GuruAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.wosyzt.myudemyspring5guruapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUdemySpring5GuruAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUdemySpring5GuruAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wosyzt/myudemyspring5guruapp/MyUdemySpring5GuruAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wosyzt.myudemyspring5guruapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUdemySpring5GuruAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
